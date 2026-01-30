77c4a31bdca4f802462798e39130c91f2e20ddbd
Anis BEN JEMIA <anis.ben-jemia@etu.univ-lyon1.fr>
2026-01-30 20:25:12 +0100

    Structure améliorée, tailwind installé, pages et composants prévus plus tard ajoutés et page d'accueil de base créée

---
+++ src/components/pages/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="flcol items-center w-dvw my-4">
+    <div class="border-2 border-amber-300">
+      <h2 class="text-2xl font-bold">Bienvenu au tutoriel VueJS</h2>
+    </div>
+    <div class="flcol items-start">
+      <p class="underline text-left">Follow a simple programs with results</p>
+      <p class="underline text-left">Get fit, healthy and strong</p>
+      <p class="underline text-left">Register now !</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
